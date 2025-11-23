d3378017d66e37122f53ea6790e96b6811fa5883
eiza <muhamadeiza29@gmail.com>
2025-11-23 18:34:24 +0700

    23/11/2025 Menambahkan halaman lupa password

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -46,4 +46,12 @@ class AuthProvider extends ChangeNotifier {
       return e.toString();
     }
   }
+
+  Future<void> forgotPassword(String email) async {
+    try {
+      await Supabase.instance.client.auth.resetPasswordForEmail(email);
+    } catch (e) {
+      throw Exception("Gagal mengirim reset password: $e");
+    }
+  }
 }
